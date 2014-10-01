02e6a32bd5d997ef1458d16a76884acd66b1f284
Santiago-vdk <Santiago.vdk@gmail.com>
2014-09-30 23:52:29 -0600

    Cambio toolbar

--- ventanaPrincipal.cpp
+++ ventanaPrincipal.cpp
@@ -89,14 +89,21 @@ void ventanaPrincipal::ventanaMostrado()
 
     QPixmap newpix(":/recursos/new.png");
     QPixmap openpix(":/recursos/open.png");
+    QPixmap savepix(":/recursos/save.png");
     QPixmap quitpix(":/recursos/quit.png");
 
     QToolBar *toolbar = addToolBar("ToolBar");
     toolbar->addAction(QIcon(newpix), "New File");
     toolbar->addAction(QIcon(openpix), "Open File");
+    toolbar->addAction(QIcon(savepix), "Save File");
     toolbar->addSeparator();
     QAction *quit = toolbar->addAction(QIcon(quitpix),
         "Quit Application");
+
+//    QAction *save = toolbar->addAction(QIcon(savepix),
+//        "Quit Application");
+
+
 
     connect(quit, SIGNAL(triggered()), qApp, SLOT(quit()));
 
