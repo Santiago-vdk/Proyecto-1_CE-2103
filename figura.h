e0bc740c42f9ff80d300ed77ab07efc0ddfb839b
rafangel <rafangel1228@gmail.com>
2014-10-02 14:52:38 -0600

    incorpore gets/sets y cambios menores

--- figura.h
+++ figura.h
@@ -1,16 +1,21 @@
 #ifndef FIGURA_H
 #define FIGURA_H
-#include <cstdlib>
+#include <string>
 
 using namespace std;
 class figura
 {
 public:
-    figura(int pNombre, int pVertices);
+    figura(string pNombre, int pVertices);
+    int getVertices();
+    string getNombre();
+    void setVertices(int vertices);
+    void setNombre(string nombre);
+
 
 
 private:
-    int _Nombre;
+    string _Nombre;
     int _Vertices;
 };
 
